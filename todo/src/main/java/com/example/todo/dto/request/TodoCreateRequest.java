56f25ef5a1e659a406a5dfb0924c47804c03635b
J <air92323@gamil.com>
2026-09-23 01:54:29 +0900

    [FIX] Request Dto NotBlack -> NotNull 로 수정

--- todo/src/main/java/com/example/todo/dto/request/TodoCreateRequest.java
+++ todo/src/main/java/com/example/todo/dto/request/TodoCreateRequest.java
@@ -1,6 +1,7 @@
 package com.example.todo.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record TodoCreateRequest(
@@ -8,7 +9,7 @@ public record TodoCreateRequest(
         @Size(max = 100, message = "Title cannot exceed 100 characters")
         String title,
 
-        @NotBlank(message = "Description cannot be blank")
+        @NotNull(message = "Completed cannot be null")
         boolean completed
 ) {
 
